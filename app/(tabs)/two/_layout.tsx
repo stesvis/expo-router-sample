45710d8b5eff9dadf3f4e97a9832d9f1894f8ebd
Cristian Merli <stesvis@gmail.com>
2024-06-24 16:20:24 -0600

    Set initial route

--- app/(tabs)/two/_layout.tsx
+++ app/(tabs)/two/_layout.tsx
@@ -3,7 +3,9 @@ import { Stack } from "expo-router";
 type TabTwoLayoutPageProps = {};
 
 const TabTwoLayoutPage = (props: TabTwoLayoutPageProps) => {
-  return <Stack screenOptions={{ headerShown: false }} />;
+  return (
+    <Stack initialRouteName="two" screenOptions={{ headerShown: false }} />
+  );
 };
 
 export default TabTwoLayoutPage;
